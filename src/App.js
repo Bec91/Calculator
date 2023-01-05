64fa48856f4f04b296912e786f4fac0bec1712f0
Becca <rkinnee91@hotmail.com>
2023-01-05 11:39:45 -0800

    correct scss layout to reflect better scss practices

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div className="calculator-grid">
-        <div className="output">
-          <div className="output__prevOperand">123,456 *</div>
-          <div className="output__currentOperand">123,456</div>
+      <div className="calculatorGrid">
+        <div className="calculatorGrid__output">
+          <div className="calculatorGrid__previous">123,456 *</div>
+          <div className="calculatorGrid__current">123,456</div>
         </div>
-        <button className="calculator-grid__btn span-two">AC</button>
-        <button className="calculator-grid__btn">DEL</button>
-        <button className="calculator-grid__btn">/</button>
-        <button className="calculator-grid__btn">1</button>
-        <button className="calculator-grid__btn">2</button>
-        <button className="calculator-grid__btn">3</button>
-        <button className="calculator-grid__btn">*</button>
-        <button className="calculator-grid__btn">4</button>
-        <button className="calculator-grid__btn">5</button>
-        <button className="calculator-grid__btn">6</button>
-        <button className="calculator-grid__btn">+</button>
-        <button className="calculator-grid__btn">7</button>
-        <button className="calculator-grid__btn">8</button>
-        <button className="calculator-grid__btn">9</button>
-        <button className="calculator-grid__btn">-</button>
-        <button className="calculator-grid__btn">.</button>
-        <button className="calculator-grid__btn">0</button>
-        <button className="calculator-grid__btn span-two">=</button>      
+        <button className="calculatorGrid__btn calculatorGrid__spanTwo">AC</button>
+        <button className="calculatorGrid__btn">DEL</button>
+        <button className="calculatorGrid__btn">/</button>
+        <button className="calculatorGrid__btn">1</button>
+        <button className="calculatorGrid__btn">2</button>
+        <button className="calculatorGrid__btn">3</button>
+        <button className="calculatorGrid__btn">*</button>
+        <button className="calculatorGrid__btn">4</button>
+        <button className="calculatorGrid__btn">5</button>
+        <button className="calculatorGrid__btn">6</button>
+        <button className="calculatorGrid__btn">+</button>
+        <button className="calculatorGrid__btn">7</button>
+        <button className="calculatorGrid__btn">8</button>
+        <button className="calculatorGrid__btn">9</button>
+        <button className="calculatorGrid__btn">-</button>
+        <button className="calculatorGrid__btn">.</button>
+        <button className="calculatorGrid__btn">0</button>
+        <button className="calculatorGrid__btn calculatorGrid__spanTwo">=</button>      
       </div>
     </div>
   );
